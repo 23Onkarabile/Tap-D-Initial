c3665ef83bc8d0b61cdd05597dadb65695bb1321
23Onkarabile <onkarabiletshekoesele138@gmail.com>
2026-07-07 10:34:46 +0200

    Update firebase.service.js

--- firebase.service.js
+++ firebase.service.js
@@ -19,7 +19,9 @@ import {
 // ══ RENDER BACKEND ══
 const RENDER_BASE_URL = 'https://tap-d-initial-backend.onrender.com';
 
-export async function callTapDishFunction(functionName, data, user) {
+export async function callTapDishFunction(functionName, data) {
+  const user = auth.currentUser;
+  if (!user) throw new Error('You must be signed in to do this.');
   const idToken = await user.getIdToken();
   const res = await fetch(`${RENDER_BASE_URL}/${functionName}`, {
     method: 'POST',
